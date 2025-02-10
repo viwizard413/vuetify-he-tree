c250c758e7c86a33427df5a2def94e54cf280213
Takayasu Akashi <TakayasuAkashi@gmail.com>
2025-02-11 00:19:10 +0900

    ツリー表示の表示/非表示を切り替えられる様にしました。

--- frontend/vuetify-project/src/pages/main.vue
+++ frontend/vuetify-project/src/pages/main.vue
@@ -1,7 +1,42 @@
 <template>
   <v-container>
     <v-responseve>
-      <ConfigurationTree :treeData="treeData" />
+      <v-toolbar elevation="2">
+        <v-toolbar-title>
+          Configuration File
+          <span v-if="configurationTree.enable">
+            <v-btn
+              @click="configurationTree.enable = !configurationTree.enable"
+              prepend-icon="mdi-chevron-left"
+            >
+              close tree
+            </v-btn>
+          </span >
+          <span v-else>
+            <v-btn
+              @click="configurationTree.enable = !configurationTree.enable"
+              prepend-icon="mdi-chevron-right"
+            >
+              open tree
+            </v-btn>
+          </span >
+        </v-toolbar-title>
+      </v-toolbar>
+      <v-row class="ma-0">
+        <v-col
+          v-if="configurationTree.enable"
+          :cols="configurationTree.enable? 3 : 0"
+        >
+          <v-card>
+            <ConfigurationTree :treeData="treeData" class="ma-2"/>
+          </v-card>
+        </v-col>
+        <v-col :cols="configurationTree.enable? 9 : 12">
+          <v-card>
+            main
+          </v-card>
+        </v-col>
+      </v-row>
     </v-responseve>
   </v-container>
 </template>
@@ -10,6 +45,9 @@
   export default {
     data() {
       return {
+        configurationTree: {
+          enable: true,
+        },
         treeData: [
           {
             text: 'Projects',
